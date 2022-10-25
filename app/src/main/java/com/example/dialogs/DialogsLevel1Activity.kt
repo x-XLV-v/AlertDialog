216645e40fde2c08cf202d5c442231d6cef8893d
x-XLV-v <explay_one@mail.ru>
2022-10-25 21:44:15 +0500

    Added three new alert dialogs in dialog level 1 (single with confirmation), (multiple choice), (multiple choice with confirmation)

--- app/src/main/java/com/example/dialogs/DialogsLevel1Activity.kt
+++ app/src/main/java/com/example/dialogs/DialogsLevel1Activity.kt
@@ -2,6 +2,7 @@ package com.example.dialogs
 
 import android.content.ContentValues.TAG
 import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -17,6 +18,7 @@ class DialogsLevel1Activity: AppCompatActivity() {
     private lateinit var binding: ActivityLevel1Binding
 
     private var volume by notNull<Int>()
+    private var color by notNull<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +33,25 @@ class DialogsLevel1Activity: AppCompatActivity() {
             showSingleChoiceAlertDialog()
         }
 
+        binding.alertDialogSingleChoiceWithConfirmationButton.setOnClickListener {
+            showSingleChoiceWithConfirmationAlertDialog()
+        }
+
+        binding.alertDialogMultipleChoiceButton.setOnClickListener {
+            showMultipleChoiceAlertDialog()
+        }
+
+        binding.alertDialogMultipleChoiceWithConfirmationButton.setOnClickListener {
+            showMultipleChoiceWithConfirmationAlertDialog()
+        }
+
         volume = savedInstanceState?.getInt(KEY_VOLUME) ?: 50
+        color = savedInstanceState?.getInt(KEY_COLOR) ?: Color.RED
 
         updateUi()
     }
 
-    //----------------------------Alert Dialog Default----------------------------//
+    //---------------------------- Alert Dialog Default ----------------------------//
 
     private fun showAlertDialog() {
         val listener = DialogInterface.OnClickListener { _, which ->
@@ -72,7 +87,7 @@ class DialogsLevel1Activity: AppCompatActivity() {
         Toast.makeText(this, messageRes, Toast.LENGTH_SHORT).show()
     }
 
-    //----------------------------Alert Dialog Single Choice----------------------------//
+    //---------------------------- Alert Dialog Single Choice ----------------------------//
 
     private fun showSingleChoiceAlertDialog() {
         val volumeItems = AvailableVolumeValues.createVolumeValues(volume)
@@ -91,12 +106,96 @@ class DialogsLevel1Activity: AppCompatActivity() {
         dialog.show()
     }
 
+    //------------------ Alert Dialog Single Choice With Confirmation --------------------//
+
+    private fun showSingleChoiceWithConfirmationAlertDialog() {
+        val volumeItems = AvailableVolumeValues.createVolumeValues(volume)
+        val volumeTextItems = volumeItems.values
+            .map { getString(R.string.volume_description, it) }
+            .toTypedArray()
+
+        val dialog = AlertDialog.Builder(this)
+            .setTitle(R.string.volume_setup)
+            .setSingleChoiceItems(volumeTextItems, volumeItems.currentIndex, null)
+            .setPositiveButton(R.string.volume_confirmation_button) { d, _, ->
+                val index = (d as AlertDialog).listView.checkedItemPosition
+                volume = volumeItems.values[index]
+                updateUi()
+            }
+            .create()
+        dialog.show()
+    }
+
+    //---------------------------- Alert Dialog Multiple Choice ----------------------------//
+
+    private fun showMultipleChoiceAlertDialog() {
+        val colorItems = resources.getStringArray(R.array.colors)
+        val colorComponents = mutableListOf(
+            Color.red(this.color),
+            Color.green(this.color),
+            Color.blue(this.color)
+        )
+
+        val checkboxes = colorComponents
+            .map { it > 0 }
+            .toBooleanArray()
+
+        val dialog = AlertDialog.Builder(this)
+            .setTitle(R.string.volume_setup)
+            .setMultiChoiceItems(colorItems, checkboxes) { _, which, isChecked ->
+                colorComponents[which] = if (isChecked) 255 else 0
+                this.color = Color.rgb(
+                    colorComponents[0],
+                    colorComponents[1],
+                    colorComponents[2]
+                )
+                updateUi()
+            }
+            .setPositiveButton(R.string.action_close, null)
+            .create()
+        dialog.show()
+    }
+
+    //------------------ Alert Dialog Multiple Choice With Confirmation --------------------//
+
+    private fun showMultipleChoiceWithConfirmationAlertDialog() {
+        val colorItems = resources.getStringArray(R.array.colors)
+        val colorComponents = mutableListOf(
+            Color.red(this.color),
+            Color.green(this.color),
+            Color.blue(this.color)
+        )
+
+        val checkboxes = colorComponents
+            .map { it > 0 }
+            .toBooleanArray()
+
+        var color: Int = this.color
+        val dialog = AlertDialog.Builder(this)
+            .setTitle(R.string.volume_setup)
+            .setMultiChoiceItems(colorItems, checkboxes) { _, which, isChecked ->
+                colorComponents[which] = if (isChecked) 255 else 0
+                color = Color.rgb(
+                    colorComponents[0],
+                    colorComponents[1],
+                    colorComponents[2]
+                )
+            }
+            .setPositiveButton(R.string.action_confirm) { _, _ ->
+                this.color = color
+                updateUi()
+            }
+            .create()
+        dialog.show()
+    }
 
     private fun updateUi() {
         binding.currentVolumeTextView.text = getString(R.string.current_volume, volume)
+        binding.colorView.setBackgroundColor(color)
     }
 
     companion object {
         @JvmStatic private val KEY_VOLUME = "KEY_VOLUME"
+        @JvmStatic private val KEY_COLOR = "KEY_COLOR"
     }
 }
